01d5db0d06448f02c41e66b9e0adbcac9971e20c
simmifoundationmadhu <115585385+simmifoundationmadhu@users.noreply.github.com>
2022-10-18 18:21:58 +0530

    Check

--- simmibackend/router.py
+++ simmibackend/router.py
@@ -3,6 +3,6 @@ from rest_framework import routers
 
 
 router=routers.DefaultRouter()
-router.register('donation',DonateViewset)
+router.register('donation2',DonateViewset)
 router.register('payment_method',PaymentViewset)
 router.register('payment_details',PaymentDetailViewset)
