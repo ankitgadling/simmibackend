3a1d1764d3e5cc50beb0fe7d23164dda581b7881
simmifoundationmadhu <115585385+simmifoundationmadhu@users.noreply.github.com>
2022-10-20 00:23:35 +0530

    Update settings.py

--- simmibackend/settings.py
+++ simmibackend/settings.py
@@ -90,8 +90,8 @@ WSGI_APPLICATION = 'simmibackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite',
-        'NAME': BASE_DIR/'db.sqlite',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR/'db.sqlite3',
     }
 }
 
