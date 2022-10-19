2d69201f125e3d547536bb3479cd5c0b69f0d3bd
simmifoundationmadhu <115585385+simmifoundationmadhu@users.noreply.github.com>
2022-10-20 01:02:51 +0530

    Update settings.py

--- simmibackend/settings.py
+++ simmibackend/settings.py
@@ -69,7 +69,7 @@ ROOT_URLCONF = 'simmibackend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR,'temps/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
