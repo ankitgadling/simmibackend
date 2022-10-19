11ccbf46c4aac3692e6e7e72efced8ed864fe3df
simmifoundationmadhu <115585385+simmifoundationmadhu@users.noreply.github.com>
2022-10-19 23:22:21 +0530

    database changed

--- simmibackend/settings.py
+++ simmibackend/settings.py
@@ -90,14 +90,14 @@ WSGI_APPLICATION = 'simmibackend.wsgi.application'
 
 DATABASES = {
     'default': {
-        
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-      
-
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'freedb_Djangotestdb',
+        'HOST':'sql.freedb.tech',
+        'PORT':'3306',
+        'USER':'freedb_simmi',
+        'PASSWORD':'p@t6zQQjJrMxE%v'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
