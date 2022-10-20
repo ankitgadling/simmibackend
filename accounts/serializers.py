0f85c2e926042d3a2c8c63cbcdcd2e5cdb824976
simmifoundationmadhu <115585385+simmifoundationmadhu@users.noreply.github.com>
2022-10-20 16:14:17 +0530

    added accounts

---
+++ accounts/serializers.py
@@ -0,0 +1,33 @@
+from pyexpat import model
+from rest_framework import serializers
+from accounts.models import registration
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+
+class Registrationserializers(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username','email', 'password']
+
+    def update(self, instance, validated_data):
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.email = validated_data.get('email', instance.email)
+        instance.username = validated_data.get('username', instance.username)
+        instance.save()
+        return instance
+
+class logindetailserializers(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    def create(self, data):
+        user = authenticate(**data)
+        if user and user.is_active:
+            return user
+        raise serializers.ValidationError("Incorrect Username or Password")
+
+class accountserializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'username','email']
---
+++ accounts/urls.py
@@ -0,0 +1,10 @@
+from .views import register_api, Login_api, user
+from knox import views as knox_views
+from django.urls import path
+
+urlpatterns = [
+    path('register/', register_api.as_view(), name='register'),
+    path('login/', Login_api.as_view(), name='login'),
+    path('logout/', knox_views.LogoutView.as_view(), name='logout'),
+    path('user/', user, name='user'),
+]
