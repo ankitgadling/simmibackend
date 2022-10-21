ddc660f76f9cd8561ad4bb85231e51c5a3f644f0
simmifoundationmadhu <115585385+simmifoundationmadhu@users.noreply.github.com>
2022-10-21 22:50:29 +0530

    updated settings for gallery app

--- simmibackend/settings.py
+++ simmibackend/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'carrers',
     'knox',
     'blog',
+    'galleryapp',
 ]
 
 MIDDLEWARE = [
@@ -136,9 +137,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+import os.path
 
-# MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = 'media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
