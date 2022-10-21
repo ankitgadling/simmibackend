be46dbbece9a66c1a146b7f8731b7a85ac52fd81
simmifoundationmadhu <115585385+simmifoundationmadhu@users.noreply.github.com>
2022-10-22 03:15:37 +0530

    Update serializers.py

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -7,7 +7,7 @@ from django.contrib.auth import authenticate
 class Registrationserializers(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id' , 'email', 'password']
+              fields = ['first_name', 'last_name', 'username','email', 'password']
         extra_kwargs = {
             'password': {'write_only': True}
         }
