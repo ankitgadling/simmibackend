a1ca93f15b0457198d73636e815369dd96085dcf
simmifoundationmadhu <115585385+simmifoundationmadhu@users.noreply.github.com>
2022-10-21 22:45:44 +0530

    added gallery app

--- galleryapp/models.py
+++ galleryapp/models.py
@@ -9,19 +9,19 @@ class education(models.Model):
 
 class livelihood(models.Model):
     title = models.CharField(max_length=50)
-    photo = models.ImageField(upload_to = "gallery/Education")
+    photo = models.ImageField(upload_to = "gallery/livelihood")
     content = models.TextField()
     
 
 
 class medical_camps(models.Model):
     title = models.CharField(max_length=50)
-    photo = models.ImageField(upload_to = "gallery/Education")
+    photo = models.ImageField(upload_to = "gallery/medical_camps")
     content = models.TextField()
     
 
 class women_empowerment(models.Model):
     title = models.CharField(max_length=50)
-    photo = models.ImageField(upload_to = "gallery/Education")
+    photo = models.ImageField(upload_to = "gallery/women_empowerment")
     content = models.TextField()
     
