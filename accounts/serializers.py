d9267615648ede5c040e0e60ad5c6fcc80e519dd
simmifoundationmadhu <115585385+simmifoundationmadhu@users.noreply.github.com>
2022-10-22 03:09:52 +0530

    Update serializers.py

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -7,14 +7,16 @@ from django.contrib.auth import authenticate
 class Registrationserializers(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['first_name', 'last_name', 'username','email', 'password']
+        fields = ['id', 'name', 'email', 'password']
+        extra_kwargs = {
+            'password': {'write_only': True}
+        }
 
-    def update(self, instance, validated_data):
-        instance.first_name = validated_data.get('first_name', instance.first_name)
-        instance.last_name = validated_data.get('last_name', instance.last_name)
-        instance.email = validated_data.get('email', instance.email)
-        instance.username = validated_data.get('username', instance.username)
-        instance.password = validated_data.get('password', instance.password)
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
         instance.save()
         return instance
 
