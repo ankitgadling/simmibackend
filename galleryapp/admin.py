db86f1b39d3efd76431a1946f3f5936b84221cda
simmifoundationmadhu <115585385+simmifoundationmadhu@users.noreply.github.com>
2022-10-22 14:54:04 +0530

    Delete galleryapp directory

--- galleryapp/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from galleryapp.models import *
-# Register your models here.
-admin.site.register(education)
-admin.site.register(livelihood)
-admin.site.register(women_empowerment)
-admin.site.register(medical_camps)
