cac28e5c95d97b88bf3eb11b1f3017f15efa74e8
simmifoundationmadhu <115585385+simmifoundationmadhu@users.noreply.github.com>
2022-10-27 01:27:26 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,8 @@ from knox import views as knox_views
 from django.urls import path
 
 urlpatterns = [
-    path('register/', register_api.as_view(), name='register'),
-    path('login/', Login_api.as_view(), name='login'),
-    path('logout/', knox_views.LogoutView.as_view(), name='logout'),
+    path('register', register_api.as_view(), name='register'),
+    path('login', Login_api.as_view(), name='login'),
+    path('logout', knox_views.LogoutView.as_view(), name='logout'),
     path('user/', user, name='user'),
 ]
