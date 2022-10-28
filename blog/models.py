d0d12cb89cd2e9639c18a20c471fef1003c2447b
HarsH <harshgolu82@gmail.com>
2022-10-28 18:46:02 +0530

    update blog-models

--- blog/models.py
+++ blog/models.py
@@ -15,7 +15,7 @@ class Blog(models.Model):
     title = models.CharField(max_length=50)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     content = models.TextField()
-    image = models.ImageField(upload_to='media/blogs')
+    image = models.ImageField(upload_to='blogs')
     date_posted = models.DateField(auto_now_add=True)
 
     def __str__(self) -> str:
