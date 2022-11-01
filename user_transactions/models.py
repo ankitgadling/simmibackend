34c4eb768d17bd6a810f4156f56f98f05bcf8921
simmifoundationmadhu <115585385+simmifoundationmadhu@users.noreply.github.com>
2022-11-02 02:26:33 +0530

    Update models.py

--- user_transactions/models.py
+++ user_transactions/models.py
@@ -1,10 +1,10 @@
 from django.db import models
-from django.contrib.auth.models import User
+from account.models import SimmiUser
 # Create your models here.
 
 class user_transactions(models.Model):
     date = models.DateField(auto_now_add=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(SimmiUser, on_delete=models.CASCADE)
     account_no = models.CharField(max_length=20)
     amount = models.IntegerField(default=100)
     status = models.CharField(max_length=20,default="pending")
