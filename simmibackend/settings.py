ba2ea3faa14b9f64b6c27081f2c6ff00a573d3bc
simmifoundationmadhu <115585385+simmifoundationmadhu@users.noreply.github.com>
2022-11-02 01:21:47 +0530

    changed settings for new auth model

--- simmibackend/settings.py
+++ simmibackend/settings.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
+    'account',
     'contact',
     'corsheaders',
     'donate',
@@ -155,3 +155,4 @@ MEDIA_URL = 'media/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'account.SimmiUser'
