2d6f14ff6e0422db17e5846a9de3c8dad98b640d
simmifoundationmadhu <115585385+simmifoundationmadhu@users.noreply.github.com>
2022-11-02 20:45:11 +0530

    Delete account directory

--- account/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from account.models import SimmiUserDetails
-
-class UserModelAdmin(admin.ModelAdmin):
-     list_display = ('user', 'first_name','last_name','ph_no' )
-admin.site.register(SimmiUserDetails,UserModelAdmin)
