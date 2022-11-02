af9e7f17ce72a6f3cd5a02cb6786edf96e00c12f
simmifoundationmadhu <115585385+simmifoundationmadhu@users.noreply.github.com>
2022-11-02 20:08:49 +0530

    Update models.py

--- latestnews/models.py
+++ latestnews/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-from account.models import SimmiUser
-
+from django.contrib.auth.models import User
 class LatestNews(models.Model):
     headline = models.CharField(max_length=100)
     description = models.TextField()
-    author = models.ForeignKey(SimmiUser, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     image = models.ImageField(upload_to='latestnews', blank=True)
     posted_date = models.DateField(auto_now_add=True)
 
