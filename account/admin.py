07a08badc0f8665248c5b6e764cc20d054feed74
simmifoundationmadhu <115585385+simmifoundationmadhu@users.noreply.github.com>
2022-11-02 20:05:25 +0530

    Delete account directory

--- account/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from account.models import SimmiUser
-
-class UserModelAdmin(admin.ModelAdmin):
-    list_display = ('email', 'first_name','last_name','ph_no', 'is_admin')
-admin.site.register(SimmiUser,UserModelAdmin)
--- account/urls.py
+++
@@ -1,9 +0,0 @@
-from .views import register_api, Login_api
-from knox.views import LogoutView
-from django.urls import path
-
-urlpatterns = [
-    path('register', register_api.as_view(), name='register'),
-    path('login', Login_api.as_view(), name='login'),
-    path('logout', LogoutView.as_view(), name='logout'),
-]
