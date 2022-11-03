17f08b551f207135c89556e5bc377da686072da5
simmifoundationmadhu <115585385+simmifoundationmadhu@users.noreply.github.com>
2022-11-03 22:59:56 +0530

    Delete userprofile directory

--- userprofile/urls.py
+++
@@ -1,7 +0,0 @@
-from .views import List_users_pisc, profileDetail
-from django.urls import path
-
-urlpatterns = [
-    path('user/', List_users_pisc.as_view(), name='users'),
-    path('user/<int:pk>/', profileDetail.as_view(), name='user')
-]
