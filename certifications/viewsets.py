98cd02262bea5dcb9637d7e2568cce246decec54
simmifoundationmadhu <115585385+simmifoundationmadhu@users.noreply.github.com>
2022-11-04 00:51:18 +0530

    Delete certifications directory

--- certifications/viewsets.py
+++
@@ -1,9 +0,0 @@
-from donate import serializers
-from rest_framework import viewsets
-from .models import certfication
-from . import serilaizers
-
-class CertificateViewset(viewsets.ModelViewSet):
-    
-    queryset=certfication.objects.all()
-    serializer_class=serilaizers.certificationSerializer
