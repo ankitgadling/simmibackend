1c4206dc6be9a031cb20138f8a6c958057ba6e15
MadhuSuniL <bagammagarimadhu@gmail.com>
2022-11-06 19:54:40 +0530

    small change

--- admin_details/serializers.py
+++ admin_details/serializers.py
@@ -4,5 +4,5 @@ from django.contrib.auth.models import User
 class AdminSerializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ['id','first_name',"last_name",'username']
+        fields = ['id','first_name',"last_name"]
         
