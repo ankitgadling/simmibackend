f1116f1d604b3ce5e4903e4930811d3fa63ba2db
Harsh <harshgolu82@gmail.com>
2022-11-10 17:07:39 +0530

    Update speaker

--- speaker/serializer.py
+++ speaker/serializer.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 from .models import *
+from drf_extra_fields.fields import Base64ImageField
+
 
 class SpeakerSerializer(serializers.ModelSerializer):
+    speaker_profile = Base64ImageField(
+        max_length=None, use_url=True,
+    )
     class Meta:
         model = Speaker
         fields = '__all__'
