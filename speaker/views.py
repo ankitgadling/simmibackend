0632130d92a1bb940ebbebc5c06c193192534918
HarsH <harshgolu82@gmail.com>
2022-11-15 23:24:07 +0530

    Add parsers

--- speaker/views.py
+++ speaker/views.py
@@ -1,18 +1,22 @@
 from .models import *
 from .serializer import *
 from rest_framework.generics import ListAPIView, CreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.parsers import MultiPartParser, FormParser
 from rest_framework.response import Response
 
 
 class AllSpeakers(ListAPIView):
+    parser_classes = (MultiPartParser, FormParser)
     queryset = Speaker.objects.all()
     serializer_class = SpeakerSerializer
 
 class PostSpeaker(CreateAPIView):
+    parser_classes = (MultiPartParser, FormParser)
     queryset = Speaker.objects.all()
     serializer_class = SpeakerSerializer
 
 
 class CRUDSpeaker(RetrieveUpdateDestroyAPIView):
+    parser_classes = (MultiPartParser, FormParser)
     queryset = Speaker.objects.all()
     serializer_class = SpeakerSerializer
