3638847fc1b219e7d7a7f13aa2cef5e9e2d0fd41
MadhuSuniL <bagammagarimadhu@gmail.com>
2022-11-15 21:43:49 +0530

    cheking tokens

--- user_transactions/views.py
+++ user_transactions/views.py
@@ -2,9 +2,14 @@ from django.shortcuts import render
 from rest_framework.generics import ListAPIView
 from .serializers import user_transactionserializer
 from .models import user_transactions
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
 # Create your views here.
 
 class user_transactionsview(ListAPIView):
     queryset = user_transactions.objects.all()
     serializer_class = user_transactionserializer
-    
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+        
