d885ac683ab7f2b17bb3d6161d345837e3a40824
MadhuSuniL <bagammagarimadhu@gmail.com>
2022-11-18 15:24:52 +0530

    updated transaction api for admin

--- admin_transactions/views.py
+++ admin_transactions/views.py
@@ -1,26 +1,25 @@
 from django.shortcuts import render
-from user_transactions.models import user_transactions
+from Razorpay.models import Transactions
 from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
 from rest_framework.serializers import ModelSerializer 
 from django.contrib.auth.models import User
-
 # Create your views here.
 
 class Userserializer(ModelSerializer):
     class Meta:
         model = User
-        fields = ["first_name","last_name"]
+        fields = ["username","first_name","last_name"]
 
 
 
-class user_transactionserializer(ModelSerializer):
+class TransactionSerializer(ModelSerializer):
     user = Userserializer(read_only=True)
     class Meta:
-        model = user_transactions
+        model = Transactions
         fields = "__all__"
         
 
 class admin_transactions_view(ListAPIView):
-    queryset = user_transactions.objects.all()
-    serializer_class = user_transactionserializer
+    queryset = Transactions.objects.all()
+    serializer_class = TransactionSerializer
