f1e1ef396fdcd69fb319fd1821b2830852d7410a
simmifoundationmadhu <115585385+simmifoundationmadhu@users.noreply.github.com>
2022-11-20 21:48:56 +0530

    Update serializers.py

--- events/serializers.py
+++ events/serializers.py
@@ -3,6 +3,9 @@ from .models import *
 from drf_extra_fields.fields import HybridImageField
 
 class EventSerializer(serializers.ModelSerializer):
+    image_1 = HybridImageField()
+    image_2 = HybridImageField()
+    image_3 = HybridImageField()
     class Meta:
         model = Event
         fields = "__all__"    
