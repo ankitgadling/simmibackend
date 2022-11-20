af6949f4d7bcc8ca4cd21e97c27c292b9e885926
simmifoundationmadhu <115585385+simmifoundationmadhu@users.noreply.github.com>
2022-11-20 22:39:13 +0530

    Update urls.py

--- events/urls.py
+++ events/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 urlpatterns = [
     path("getallevents/",AllEvents.as_view()),
-    path('eventcreate',EventCreateView.as_view()),
+    path('eventcreate/',EventCreateView.as_view()),
     path('eventmodify/<pk>',EventModifyView.as_view()),
 ]
