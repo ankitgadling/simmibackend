82400d925f902cf23da036779ed6c5614fb0480f
MadhuSuniL <bagammagarimadhu@gmail.com>
2022-11-24 00:42:15 +0530

    minor changes

--- admin_logs/urls.py
+++ admin_logs/urls.py
@@ -5,9 +5,9 @@ from rest_framework_simplejwt.views import TokenBlacklistView
 
 urlpatterns = [
     path('login',admin_login.as_view()),
-    path('logout',admin_logout.as_view()),
+    #path('logout',admin_logout.as_view()),
     path('change_password',ChangePassword.as_view()),
     path('get_token_by_user/', TokenObtainPairView.as_view()),
     path('token_refresh/', TokenRefreshView.as_view()),
-    path('token_delete/', TokenBlacklistView.as_view()), 
+    path('logout', TokenBlacklistView.as_view()), 
 ]
