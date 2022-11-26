d2cb1ae77ce5d7dbd08be3819738bfc2f04a87d7
Harsh <harshgolu82@gmail.com>
2022-11-26 16:32:32 +0530

    Add story of change & Awards and Recognition

--- get_involved/views.py
+++ get_involved/views.py
@@ -6,6 +6,7 @@ class IndividualSupporterView(viewsets.ModelViewSet):
     queryset = IndividualSupporter.objects.all()
     serializer_class = IndividualSupporterSerializer
 
+
 class PressMediaView(viewsets.ModelViewSet):
     queryset = PressMedia.objects.all()
     serializer_class = PressMediaSerializer
@@ -20,6 +21,19 @@ class EminentPersonalityView(viewsets.ModelViewSet):
     queryset = EminentPersonality.objects.all()
     serializer_class = EminentPersonalitySerializer
 
+
 class PublicationView(viewsets.ModelViewSet):
     queryset = Publication.objects.all()
     serializer_class = PublicationSerializer
+
+
+class AwardsRecognitionView(viewsets.ModelViewSet):
+    queryset = AwardsRecognition.objects.all()
+    serializer_class = AwardsRecognitionSerializer
+
+
+class StoryOfChangeView(viewsets.ModelViewSet):
+    queryset = StoryOfChange.objects.all()
+    serializer_class = StoryOfChangeSerializer
+    
+    
