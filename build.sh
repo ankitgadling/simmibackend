638dcc53bbca1fffe2e5b67b48709d9bbf8e1f41
MadhuSuniL <bagammagarimadhu@gmail.com>
2022-11-28 01:39:40 +0530

    minor

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#build the project
-
-echo "Building..........."
-python --version
-pip install -r requirements.txt
-python manage.py collectstatic --noinput --clear
--- generate_certificate/views.py
+++ generate_certificate/views.py
@@ -9,7 +9,9 @@ from PIL import ImageDraw,ImageFont
 import io
 import os
 import sys
+import img2pdf
 import urllib.request
+from django.core.files import File
 from django.core.files.uploadedfile import InMemoryUploadedFile
         
 
@@ -27,34 +29,54 @@ class Genarate(GenericAPIView):
         username = user.username
         #values for certificate
         name = str(first_name+" "+last_name).upper()
+        if len(name) > 19:
+            name = first_name.upper()
         event_name = str(event.event_name).upper()
-        date = event.time.date().strftime("%d/%b/%Y")
+        date = event.time.date().strftime("%d-%b-%Y")
         file_name = username+"_"+event_name
-        
-        urllib.request.urlretrieve('https://res.cloudinary.com/dcc8pmavm/image/upload/v1669013499/media/static_files/Certificate_template_uz1pc1.jpg',"certificate.jpg")
+        l1 ="simmifoundation sample text for genarate certification for event of simmifoundation" 
+        l2 ="genarate certification for event of simmifoundation simmifoundation sample text for " 
+        l3 ="certification for event of simmifoundation sample text for genarate " 
+        urllib.request.urlretrieve('https://res.cloudinary.com/dcc8pmavm/image/upload/v1669456349/media/static_files/Picsart_22-11-26_15-18-01-572_gvdunr.jpg',"certificate.jpg")
         img = Img.open("certificate.jpg")
         urllib.request.urlretrieve('https://res.cloudinary.com/dcc8pmavm/raw/upload/v1669015852/media/static_files/Arial_wwaooe.ttf',"Arial.ttf")
         font = ImageFont.truetype("Arial.ttf",70)
-        font2 = ImageFont.truetype("Arial.ttf",35)
+        font2 = ImageFont.truetype("Arial.ttf",27)
+        font3 = ImageFont.truetype("Arial.ttf",24)
         draw = ImageDraw.Draw(img)
-        draw.text((740,700), name,(255,165,0),font=font)
-        draw.text((375,1046), date,(0,0,0),font=font2)
-        draw.text((1187,833), event_name,(0,140,0),font=font2)
-        image = img
-        output = io.BytesIO()
-        image.save(output, format='JPEG', quality=85)
-        output.seek(0)
-        new_pic= InMemoryUploadedFile(output, 'ImageField',
-                                        file_name,
-                                        'image/jpeg',
-                                        sys.getsizeof(output), None)
+        draw.text((733,490), name.center(19),(255,165,0),font=font)
+        draw.text((700,608), l1,(105,105,105),font=font3)
+        draw.text((700,648), l2,(105,105,105),font=font3)
+        draw.text((700,688), l3,(105,105,105),font=font3)
+        draw.text((950,730), "Completed On "+date,(0,0,0),font=font2)
+        draw.text((950,730), "Completed On "+date,(0,0,0),font=font2)
+        #draw.text((1187,833), event_name,(0,140,0),font=font2)
+        img.save(f"{file_name}.jpg")
+        image = Img.open("certificate.jpg")
+        pdf_file = img2pdf.convert(image.filename)
+        file = open(f"{file_name}.pdf","wb")
+        file.write(pdf_file)
+        file.close()
+        
+        pdf = File(open(f"{file_name}.pdf"))
+        # image = file
+        # output = io.BytesIO()
+        # image.save(output, format='pdf', quality=85)
+        # output.seek(0)
+        # new_pic= InMemoryUploadedFile(output, 'FileField',
+        #                                 file_name,
+        #                                 'pdf/txt',
+        #                                 sys.getsizeof(output), None)
         #values for create object of certificate
         event_name2 = event.event_name
         mentor_name = event.speaker_name
         date2 = event.time.date()
+        print(type(pdf))
+        print(pdf.name)
+        return Response("DOne")
         
         crt = certfication.objects.create(
-            event_name=event_name2,mentor_name=mentor_name,issue_date=date2,img=new_pic,status="Not Completed",user=user
+            event_name=event_name2,mentor_name=mentor_name,issue_date=date2,img=pdf,status="Not Completed",user=user
         )
         try:
             del request.session[str(username)]
