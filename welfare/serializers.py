5ec49ce6b1e058b685bcec2100a5b70967b17f5d
Harsh <harshgolu82@gmail.com>
2022-12-01 01:34:29 +0530

    update serializer

--- welfare/serializers.py
+++ welfare/serializers.py
@@ -1,3 +1,4 @@
+from unicodedata import category
 from rest_framework import serializers
 from .models import *
 from drf_extra_fields.fields import HybridImageField
@@ -6,7 +7,7 @@ class CategorySerializer(serializers.ModelSerializer):
     
     class Meta:
         model = Category
-        fields = '__all__'
+        fields = ['category']
 
     
 class BlogSerializer(serializers.ModelSerializer):
@@ -14,3 +15,8 @@ class BlogSerializer(serializers.ModelSerializer):
     class Meta:
         model = Blog
         fields = '__all__'
+
+    def to_representation(self, instance):
+        category_name = super().to_representation(instance)
+        category_name['category'] = CategorySerializer(instance.category).data
+        return category_name
