d93843faed25809c03b15e4119ad6ca8b63c5a37
Harsh <harshgolu82@gmail.com>
2022-12-18 20:30:18 +0530

    modify gallery

--- gallery/serializers.py
+++ gallery/serializers.py
@@ -5,12 +5,18 @@ from drf_extra_fields.fields import HybridImageField
 
 class Galleryserializers(ModelSerializer):
     photo = HybridImageField()
+    photo2 = HybridImageField()
+    photo3 = HybridImageField()
+    
     class Meta:
         model = Gallerytable
         fields = '__all__'     
 
 class AdminSerializer(ModelSerializer):
     photo = HybridImageField()
+    photo2 = HybridImageField()
+    photo3 = HybridImageField()
+
     class Meta:
         model = Gallerytable
         exclude = ['admin'] 
