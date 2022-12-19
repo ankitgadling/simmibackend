3284e9b55947b3587dc60023cc8ccd67980d1246
MATHIVANAN M <mathivananmv.cs20@bitsathy.ac.in>
2022-12-19 19:52:13 +0530

    some minor changes in carrers app

--- carrers/models.py
+++ carrers/models.py
@@ -17,22 +17,23 @@ class Jobs(models.Model):
     salary_range = models.CharField(max_length=50)
     level = models.CharField(max_length=50)
     posted_date = models.DateField(auto_now_add=True)
+    skills_req = models.TextField(null=True)
     description = models.TextField(max_length=60000 ,default="")
     
     def __str__(self):
         return self.title
 
 class jobappliedbyuse(models.Model):
-    how_you_heared_us=models.TextField()
+    how_you_heared_us=models.TextField(null=True)
     jobid=models.IntegerField()
     country=models.CharField(max_length=40)
     first_name=models.CharField(max_length=40)
     last_name=models.CharField(max_length=40)
-    adhar_no=models.CharField(max_length=12,validators=[MinLengthValidator(12),MaxLengthValidator(12)],null=False)
+    adhar_no=models.CharField(max_length=12,validators=[MinLengthValidator(12),MaxLengthValidator(12)],null=True)
     applied_on=models.DateField(auto_now_add=True)
-    address_lane=models.TextField()
-    city=models.CharField(max_length=30)
-    postal_code=models.CharField(max_length=30)
+    address_lane=models.TextField(null=True)
+    city=models.CharField(max_length=30,null=True)
+    postal_code=models.CharField(max_length=30,null=True)
     email=models.EmailField()
     country_code=models.CharField(max_length=5)
     mobile_number=models.CharField(max_length=16,null=False)
