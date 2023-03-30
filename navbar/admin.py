1ff1f63d12308cbaeaba936c3d9aa5e8f2dcab9c
SOORAJTS2001 <cherrycheeks404@gmail.com>
2023-03-30 15:49:32 +0530

    solved the issue of navbar api

--- navbar/admin.py
+++ navbar/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+class UpImagesAdmin(admin.ModelAdmin):
+    list_display = ['img1','img2','img3','img4']
+admin.site.register(upImages,UpImagesAdmin)
